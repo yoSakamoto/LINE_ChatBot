c7a64e0c660e1b56ec977e5bb7f5f1a25bee6f2b
yoSakamoto <sasiko.spiral@gmail.com>
2016-12-28 21:18:58 +0900

    added webhook

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var express = require('express');
 var app = express();
 
 var port = (process.env.PORT || 3000);
-var server = app.listen(port,function() {
-  console.log('Node is running on port' + port);
+var server = app.listen(port, function () {
+    console.log('Node is running on port' + port);
 });
 
-app.get('/',function(req,res,next) {
-  res.send('Node is running on port' + port);
+app.get('/', function (req, res, next) {
+    res.send('Node is running on port' + port);
 });
 
-app.post('/webhook', function(req, res, next){
+app.post('/webhook', function (req, res, next) {
     res.status(200).end();
-    console.log(req.body);
+    for (var event.type == body.events) {
+        if (evnet.type == "message") {
+            console.log(event.message);
+        }
+    }
 });
