87c41b5496d1d32c5f6f61d7b10159dd782ca175
yoSakamoto <sasiko.spiral@gmail.com>
2016-12-28 16:19:04 +0900

    added webhook

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ var server = app.listen(port,function() {
 });
 
 app.get('/',function(req,res,next) {
-  res.send('Node is running on port' + port)
+  res.send('Node is running on port' + port);
+});
+
+app.post('/webhook', function(req, res, next){
+    res.status(200).end();
+    console.log(req.body);
 });
