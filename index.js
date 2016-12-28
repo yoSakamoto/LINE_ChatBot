cf17c7c3e7ed1f168a9e91f3ae3c09a4ab01189a
yoSakamoto <sasiko.spiral@gmail.com>
2016-12-28 21:23:26 +0900

    added webhook

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res, next) {
 app.post('/webhook', function (req, res, next) {
     res.status(200).end();
     for (var event of req.body.events) {
-        if (evnet.type == "message") {
+        if (event.type == 'message') {
             console.log(event.message);
         }
     }
